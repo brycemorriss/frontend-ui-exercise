72e1c86f27563788ba876a26d838361d5b77e03f
Bryce Morriss <bryce.scott.morriss@gmail.com>
2021-04-05 17:16:56 +0930

    Add Homepage HTML5 structure, install and initialise JS plugins

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,8 +8,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Option 2
 //
@@ -17,6 +16,16 @@
 
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $('.service-icons').slick({
+        infinite: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+});
